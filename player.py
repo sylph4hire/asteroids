eb0e790183090e7f3aaed59100024b8af3473fff
sylph4hire <josph.goodwin@outlook.com>
2024-10-15 11:26:04 -0400

    F: Added player controls.

--- player.py
+++ player.py
@@ -22,5 +22,16 @@ class Player(CircleShape):
               (255,255,255),
               self.triangle(),
               2)
+         
+    def rotate(self, dt):
+         self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
 
