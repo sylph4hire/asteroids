de175cd62830ab572020148b936b98be122511d7
sylph4hire <josph.goodwin@outlook.com>
2024-10-15 11:20:56 -0400

    E: Bug fix regarding drawing player.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -8,6 +10,7 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)  # This limits the frame rate to 60 FPS
         dt = clock.get_time() / 1000  # This gets the time passed in seconds
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
-            super.__init__(x, y, PLAYER_RADIUS)
+            super().__init__(x, y, PLAYER_RADIUS)
             self.rotation = 0
     
     # in the player class
