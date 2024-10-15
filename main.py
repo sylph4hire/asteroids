9deaebf7e0cb016113e9daa53f18e175d193aaf1
sylph4hire <josph.goodwin@outlook.com>
2024-10-15 11:48:13 -0400

    H: Added asteroid field.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,15 +11,24 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     clock = pygame.time.Clock()
+    updatable= pygame.sprite.Group()
+    drawable= pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         clock.tick(60)  # This limits the frame rate to 60 FPS
         dt = clock.get_time() / 1000  # This gets the time passed in seconds
